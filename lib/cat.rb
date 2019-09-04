1c16bd1a23be6658e50201affa32ac242f67a473
yaoyu bao <yaobao5354@gmail.com>
2019-09-04 20:22:00 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood 
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name, owner, mood = "nervous")
+    @name = name 
+    @owner = owner 
+    @mood = mood
+    @@all << self 
+  end 
+  
+  #def owner 
+  #  @owner = 
+  #end 
+  
+  def self.all
+    @@all
+  end 
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,91 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_reader :name, :species, :say_species
+  
+  @@all = []
+  
+  def initialize(name, species = "human")
+    @name = name 
+    @species = species 
+    @@all << self
+  end 
+  
+  def say_species
+    "I am a #{@species}."
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.count 
+    @@all.length
+  end 
+  
+  def self.reset_all
+    @@all.clear
+  end 
+  
+  def cats
+    cats = []
+    Cat.all.each do |cat| 
+      if cat.owner == self 
+        cats << cat 
+      end 
+    end 
+    cats
+  end 
+  
+  def dogs
+    dogs = []
+    Dog.all.each do |dog| 
+      if dog.owner == self 
+        dogs << dog 
+      end 
+    end 
+    dogs
+  end 
+  
+  def buy_cat(name)
+    Cat.new(name, self)
+  end 
+  
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  
+  def walk_dogs
+    Dog.all.each do |dog|  
+      if dog.owner == self 
+        dog.mood = "happy"
+      end
+    end 
+  end 
+  
+  def feed_cats
+    Cat.all.each do |cat|  
+      if cat.owner == self 
+        cat.mood = "happy"
+      end
+    end 
+  end 
+  
+  def sell_pets 
+    Dog.all.each do |dog|  
+      if dog.owner == self 
+        dog.mood = "nervous"
+        dog.owner = nil
+      end
+    end
+    Cat.all.each do |cat|  
+      if cat.owner == self 
+        cat.mood = "nervous"
+        cat.owner = nil
+      end
+    end
+  end 
+  
+  def list_pets 
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end 
 end
